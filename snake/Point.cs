0bd5865e434d022eb3336db0e20e49fdd59d72ec
vafle <vafle@DESKTOP-K2BF7TN>
2022-03-13 21:34:37 +0200

    Add project files.

---
+++ snake/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class Point
+    {
+        //parametri
+        public int x;
+        public int y;
+        public char sym;
+
+        //constructori
+        public Point() { }
+        public Point(int X, int Y, char SYM)
+        {
+            x = X;
+            y = Y;
+            sym = SYM;
+        }
+        
+        //functions
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
---
+++ snake/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1,3,'*');
+            p1.Draw();
+
+            Point p2 = new Point(3,2,'*');
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
